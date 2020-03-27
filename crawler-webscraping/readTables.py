d2654a51a5b4a66bdaceff6c8d6121468b269015
Jefferson Sousa <lazumbra@gmail.com>
2020-03-27 20:30:31 -0300

    new function to get the tables done

--- crawler-webscraping/readTables.py
+++ crawler-webscraping/readTables.py
@@ -1,7 +1,60 @@
 import requests
 from bs4 import BeautifulSoup
-import pandas as pd
 import csv
+import calendar
+import datetime
+import time
+
+def form_table_1(tableProcess):
+    """
+    table : for table one should be table[1]
+    """
+    i = 0
+    j = 0
+    firstPtable = []
+    finalTable = []
+    for row in tableProcess.find_all('tr'):
+        i += 1
+        qunatiLinhas = len(row.find_all('td'))
+        smallTable = []
+        smallFirstT = []
+        for linhas in row.find_all('td'):
+            j+=1
+            if i < 4:
+                smallFirstT.append(linhas.text.strip())
+                
+            if(i >= 4):
+                smallTable.append(linhas.text)
+        j = 0
+        if len(smallFirstT) > 0:
+            firstPtable.append(smallFirstT)
+        if len(smallTable) > 0:
+            finalTable.append(smallTable)
+    
+    #Hora de formar a minha tabelinha final
+    tabelinhaFinal = []
+    smallVai = []
+    smallVai.append(firstPtable[0][0])
+    smallVai.append(firstPtable[0][1])
+    smallVai.append(firstPtable[0][2])
+    smallVai.append(firstPtable[0][3])
+    smallVai.append(firstPtable[2][0].split(':')[0])
+    smallVai.append(firstPtable[2][1].split(':')[0])
+
+    tabelinhaFinal.append(smallVai)
+    smallVai = []
+    smallVai.append(firstPtable[1][0])
+    smallVai.append('0'), smallVai.append('0'), smallVai.append('0')
+    smallVai.append(firstPtable[2][0].split(':')[1])
+    smallVai.append(firstPtable[2][1].split(':')[1])
+
+    tabelinhaFinal.append(smallVai)
+
+    for linha in finalTable:
+        tabelinhaFinal.append(linha)
+
+
+    return tabelinhaFinal
 
 result = requests.get("http://bvmf.bmfbovespa.com.br/market-data/html/md_stats_20200320.html")
 src = result.content
@@ -194,7 +247,64 @@ if len(table) > 0:
     for linha in tabelinhaFinal:
         print(linha)    
 
-        
-    
 
+result = requests.get("http://bvmf.bmfbovespa.com.br/market-data/html/md_stats_20200102.html")
+src = result.content
+print(src)
+print(result.status_code)
+
+
+#Criar todas as datas para o mês de janeiro.
+year = 2020
+month = 1
+JanMonth = calendar.monthcalendar(2020, 1)
+
+#loop over the month
+for i in range(len(JanMonth)):
+    for j in range(len(JanMonth[0])):
+        if JanMonth[i][j] != 0:
+            dateIs = JanMonth[i][j]
+            dayWeek = datetime.datetime(year, month, dateIs)
+            dayOfWeek = dayWeek.weekday()
+
+            #Create String to get the html
+            if dayOfWeek < 5:
+                firstPartStr = "http://bvmf.bmfbovespa.com.br/market-data/html/md_stats_" + str(year) + str(month).rjust(2, '0') + str(dateIs).rjust(2,'0') + ".html"
+
+
+                attemptives = 4
+                result = None
+
+                while attemptives > 0:
+                    attemptives -= 1
+                    result = requests.get(firstPartStr)
+                    if result.status_code == 200:
+                        attemptives = -1
+                        print(firstPartStr, dayOfWeek)
+                        src = result.content
+                        soup = BeautifulSoup(src, 'lxml')
+                        table = soup.find_all('table')
+                        print(len(table))
+                        
+                        if len(table) > 0:
+                            tableOne = table[1]
+                            tableTwo = table[3]
+                            vejaTabela = form_table_1(tableOne)
+                            print('veja a tabela')
+                            print(vejaTabela)
+                            print('\nVeja a tabela 2')
+                            vejaTabela = form_table_1(tableTwo)
+                            print(vejaTabela)
+
+
+                        print('Deu certo')
+                    time.sleep(1)
+                
+                
+                
+                
+                    
+                    
+
+            #print('Dia da semana: ', dateIs, dayOfWeek)
 

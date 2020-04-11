df5b9cf8dc2895fa3a323a4dff9d8c7c876def08
Jefferson Sousa <lazumbra@gmail.com>
2020-04-11 15:18:22 -0300

    how to to plot a bar chart

---
+++ Matplot_Tutorial_Series/barChart.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt 
+
+x = [2, 4, 6, 8, 10]
+y = [6, 7, 8, 2, 4]
+
+x1 = [1, 3, 5, 7, 9]
+y2 = [7, 8, 2, 4, 2]
+
+plt.bar(x, y, label='Bars1', color='r')
+plt.bar(x1, y2, label='Bars2', color='c')
+
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Interesting graph\nCheck it out!')
+plt.legend()
+plt.show()
